2c791edd375bb8de75ef1dd987c59dbd01f6ede2
Aarush Singh <83762450+rahulray11@users.noreply.github.com>
2024-10-01 23:57:25 +0530

    Added files

---
+++ src/main/java/com/spark/pojo/Employee.java
@@ -0,0 +1,39 @@
+package com.spark.pojo;
+
+public class Employee {
+    private int id;
+    private String name;
+    private String department;
+
+    // Constructor
+    public Employee(int id, String name, String department) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
